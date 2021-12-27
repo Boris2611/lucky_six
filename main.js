a4bbfe52fae811141b618bab4f74f56dad594188
Boris2611 <84404796+Boris2611@users.noreply.github.com>
2021-12-27 19:26:55 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
+
+var b = 0;
+
+
+
+
+// Button press
 function button(){
+    new Audio('start.wav').play()
+    
+
+    document.getElementById("dugme2").style.visibility = "hidden";
+
+  
+
+    // Zove glavnu funkciju
     rand();
+
+    
+    interval = 300;
+    myInterval = setInterval(izlazi, interval);
     
 }
 
+function stopInterval() {
+    clearInterval(myInterval);
+  }
+
+  
 
 
 var balance = 1000
+
+
 function rand(){
+    var xxx = 1
+    // 35 random brojeva
     var arr = []
     while(arr.length < 35){
       var candidateInt = Math.floor(Math.random() * 48) + 1
       if(arr.indexOf(candidateInt) === -1) arr.push(candidateInt)
-    }
+    } 
 
+
+    // Balance
     balance = balance - document.getElementById("ulog").value;
-
     kvota = ["None", "None", "None", "None", "None",
             10000, 7500, 5000, 2500, 1000, 500, 300,
             200, 150, 100, 90, 80, 70, 60, 50, 40,
@@ -25,87 +54,58 @@ function rand(){
     // Vraca boju nazad u zutu pri pocetku svake igre
     for (var a = 0; a < 35;) {
     a += 1;
-    idd = "ball" + a;
-    document.getElementById(idd).style.backgroundColor = "yellow";
-    
+    id = "ball" + a;
+    document.getElementById(id).style.visibility = "hidden";
+    document.getElementById(id).style.backgroundColor = "yellow";
+    document.getElementById(id).innerHTML = "...";
     
     }
 
-            
+    
 
 
+   
+   
     var dobitni = [];
     var dobitniIndexi = [];
     for (var i = 0; i < 35;) {
-    
         if (arr[i] == document.getElementById("br1").value) {
             dobitni.push(arr[i])
-            dobitniIndexi.push(i)
-            
+            dobitniIndexi.push(i)    
         }
-        
         else if (arr[i] == document.getElementById("br2").value) {
             dobitni.push(arr[i])
             dobitniIndexi.push(i)
         }
-
         else if (arr[i] == document.getElementById("br3").value) {
             dobitni.push(arr[i])
             dobitniIndexi.push(i)
         }
-
         else if (arr[i] == document.getElementById("br4").value) {
             dobitni.push(arr[i])
             dobitniIndexi.push(i)
         }
-
         else if (arr[i] == document.getElementById("br5").value) {
             dobitni.push(arr[i])
             dobitniIndexi.push(i)
         }
-
         else if (arr[i] == document.getElementById("br6").value) {
             dobitni.push(arr[i])
             dobitniIndexi.push(i)
         }
 
+        // Ispis gubitnih brojeva
         else {
-            document.getElementById("ball1").innerHTML = arr[0];
-            document.getElementById("ball2").innerHTML = arr[1];
-            document.getElementById("ball3").innerHTML = arr[2];
-            document.getElementById("ball4").innerHTML = arr[3];
-            document.getElementById("ball5").innerHTML = arr[4];
-            document.getElementById("ball6").innerHTML = arr[5];
-            document.getElementById("ball7").innerHTML = arr[6];
-            document.getElementById("ball8").innerHTML = arr[7];
-            document.getElementById("ball9").innerHTML = arr[8];
-            document.getElementById("ball10").innerHTML = arr[9];
-            document.getElementById("ball11").innerHTML = arr[10];
-            document.getElementById("ball12").innerHTML = arr[11];
-            document.getElementById("ball13").innerHTML = arr[12];
-            document.getElementById("ball14").innerHTML = arr[13];
-            document.getElementById("ball15").innerHTML = arr[14];
-            document.getElementById("ball16").innerHTML = arr[15];
-            document.getElementById("ball17").innerHTML = arr[16];
-            document.getElementById("ball18").innerHTML = arr[17];
-            document.getElementById("ball19").innerHTML = arr[18];
-            document.getElementById("ball20").innerHTML = arr[19];
-            document.getElementById("ball21").innerHTML = arr[20];
-            document.getElementById("ball22").innerHTML = arr[21];
-            document.getElementById("ball23").innerHTML = arr[22];
-            document.getElementById("ball24").innerHTML = arr[23];
-            document.getElementById("ball25").innerHTML = arr[24];
-            document.getElementById("ball26").innerHTML = arr[25];
-            document.getElementById("ball27").innerHTML = arr[26];
-            document.getElementById("ball28").innerHTML = arr[27];
-            document.getElementById("ball29").innerHTML = arr[28];
-            document.getElementById("ball30").innerHTML = arr[29];
-            document.getElementById("ball31").innerHTML = arr[30];
-            document.getElementById("ball32").innerHTML = arr[31];
-            document.getElementById("ball33").innerHTML = arr[32];
-            document.getElementById("ball34").innerHTML = arr[33];
-            document.getElementById("ball35").innerHTML = arr[34];
-           
+            var d = 1
+            while (d < 36) {
+                id = "ball" + d;
+                
+                document.getElementById(id).innerHTML = arr[d-1];
+                
+                
+                d +=1;
+            }
+ 
         }
 
         i += 1
@@ -113,11 +113,10 @@ function rand(){
        
       } 
 
-   
 
-
+      // Win / Lose provera
     var winlose = ""
-    let length = dobitni.length;
+    var length = dobitni.length;
     if (length == 6) {
         winlose = "DOBITAN"
         document.getElementById("win").style.color= "green";
@@ -127,12 +126,14 @@ function rand(){
         document.getElementById("win").style.color= "red";
     }
 
-    
- 
-
+    // Win / Lose prikaz
     document.getElementById("win").innerHTML = winlose
+   
 
 
+ 
+    
+    // Provera koji brojevi su dobitni i promena njihove boje
     var length2 = dobitniIndexi.length;
     y = 0
     var id = ""
@@ -149,25 +150,50 @@ function rand(){
     }
 
 
-
-            
-    if (length == 6) {
+     // Racunanje dobitka
+     if (length == 6) {
         dobitak = document.getElementById("ulog").value * kvota[dk];
-        alert("Dobitak: " + dobitak + "\n Kvota: x" + kvota[dk])
+        setTimeout(alertt, 10060)
     }
-       
 
+
+    function alertt() {
+        new Audio('win.wav').play()
+        alert("Dobitak: " + dobitak + "\n Kvota: x" + kvota[dk])
+        
+    }
 
     balance = balance + dobitak
-
-    
-
+    xxx = 0
+    // Prikaz novca za PC i telefon
     document.getElementById("winn").innerHTML ="Novac: " + balance;
     document.getElementById("winn2").innerHTML ="Novac: " + balance;
     
-    
 
     
-  }
+}
 
- 
+// timer funkcija
+function izlazi() {
+   
+    
+        
+        
+    
+
+
+    b += 1;
+    id = "ball" + b;
+    document.getElementById(id).style.visibility = "visible";
+    new Audio('click.wav').play()
+
+    if (b == 35) {
+        stopInterval();
+        
+        document.getElementById("dugme2").style.visibility = "visible";
+        return b=0;
+
+    
+
+    }
+}
